7e59fecfcbd068ef495a9c7b2eb4203f8e566b27
Michael Hartmeier <mlhartme.users.noreply@github.com>
2023-10-06 13:23:23 +0200

    code cleanup

--- src/test/java/de/schmizzolin/vish/talk/Demo.java
+++ src/test/java/de/schmizzolin/vish/talk/Demo.java
@@ -1,100 +1,53 @@
-/*
- * Copyright Michael Hartmeier, https://github.com/mlhartme/
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package de.schmizzolin.vish.talk;
 
-import java.lang.foreign.Arena;
-import java.lang.foreign.FunctionDescriptor;
-import java.lang.foreign.Linker;
-import java.lang.foreign.MemoryLayout;
-import java.lang.foreign.MemorySegment;
-import java.lang.foreign.SymbolLookup;
-import java.lang.foreign.ValueLayout;
+import java.lang.foreign.*;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 
-import static java.lang.foreign.ValueLayout.ADDRESS;
-import static java.lang.foreign.ValueLayout.JAVA_BYTE;
-import static java.lang.foreign.ValueLayout.JAVA_INT;
-import static java.lang.foreign.ValueLayout.JAVA_LONG;
-
-/**
- * https://www.gnu.org/software/libc/manual/html_node/Process-Identification.html
- *
- * Essentially <code>int getpid();</code>
- */
 public class Demo {
-    public static void main(String[] args) throws Throwable {
-        printPid();
-        printPwnam("mhm");
-
-        printSignal30();
-    }
-
     private static final Linker LINKER = Linker.nativeLinker();
 
-    public static MethodHandle downcall(String name, MemoryLayout result, MemoryLayout ... args) {
+    public static void main(String[] args) throws Throwable {
+        MethodHandle getpid = downcall("getpid", desc(ValueLayout.JAVA_INT));
+        System.out.println("pid: " + (int) getpid.invoke());
+
+        printUid(System.getProperty("user.name"));
+        MemorySegment stub = upcall("printSignal", desc(null, ValueLayout.JAVA_INT));
+        downcall("signal", desc(ValueLayout.ADDRESS, ValueLayout.JAVA_INT, ValueLayout.ADDRESS)).invoke(30, stub);
+        System.out.println("waiting ...");
+        Thread.sleep(20000);
+    }
+
+    private static MemorySegment upcall(String name, FunctionDescriptor desc) throws NoSuchMethodException, IllegalAccessException {
+        MethodHandle handle = MethodHandles.lookup().findStatic(Demo.class, name, desc.toMethodType());
+        return LINKER.upcallStub(handle, desc, Arena.global());
+    }
+
+
+    private static MethodHandle downcall(String name, FunctionDescriptor desc) {
         MemorySegment addr = LINKER.defaultLookup().find(name).get();
-        return LINKER.downcallHandle(addr, FunctionDescriptor.of(result, args));
+        return LINKER.downcallHandle(addr, desc);
     }
 
-    public static void printPid() throws Throwable {
-        MethodHandle getpid = downcall("getpid", JAVA_INT);
-        System.out.println("pid: " + getpid.invoke());
+    public static FunctionDescriptor desc(ValueLayout result, ValueLayout... args) {
+        return result == null ? FunctionDescriptor.ofVoid(args) : FunctionDescriptor.of(result, args);
     }
 
-    public static void printPwnam(String name) throws Throwable {
-        ValueLayout C_STRING = ADDRESS.withTargetLayout(MemoryLayout.sequenceLayout(JAVA_BYTE));
+    public static void printSignal(int sig) {
+        System.out.println("got signal " + sig);
+    }
+
+    public static void printUid(String name) throws Throwable {
         MemoryLayout layout = MemoryLayout.structLayout(
-                C_STRING.withName("name"),
-                C_STRING.withName("passwd"),
-                JAVA_INT.withName("uid"),
-                JAVA_INT.withName("gid"),
-                JAVA_LONG.withName("change"),
-                C_STRING.withName("class"),
-                C_STRING.withName("info"),
-                C_STRING.withName("dir"),
-                C_STRING.withName("shell")
+                ValueLayout.ADDRESS.withName("name"),
+                ValueLayout.ADDRESS.withName("passwd"),
+                ValueLayout.JAVA_INT.withName("uid")
         );
-        MethodHandle getpwnam = downcall("getpwnam", ADDRESS.withTargetLayout(layout), C_STRING);
-        MemorySegment struct = (MemorySegment) getpwnam.invoke(Arena.ofAuto().allocateUtf8String(name));
-        if (struct.address() == 0) {
-            System.out.println("not found: " + name);
-        } else {
-            MemorySegment info = (MemorySegment) layout.varHandle(MemoryLayout.PathElement.groupElement("info")).get(struct);
-            System.out.println(name + " info: " + info.getUtf8String(0));
-        }
-    }
-
-    //-- upcall
-
-    public static void printSignal30() throws Throwable {
-        MemorySegment handler = upcall(Demo.class, "signalPrinter", JAVA_INT);
-
-        MethodHandle signal = downcall("signal", ADDRESS, JAVA_INT, ADDRESS);
-        signal.invoke(30, handler);
-        Thread.sleep(10000);
-    }
-
-    public static void signalPrinter(int signal) {
-        System.out.println("got signal: " + signal);
-    }
-
-    public static MemorySegment upcall(Class<?> clazz, String method, MemoryLayout... args) throws NoSuchMethodException, IllegalAccessException {
-        FunctionDescriptor descriptor = FunctionDescriptor.ofVoid(args);
-        MethodHandle handle = MethodHandles.lookup().findStatic(clazz, method, descriptor.toMethodType());
-        return LINKER.upcallStub(handle, descriptor, Arena.global());
+        var getpwnam = downcall("getpwnam", desc(ValueLayout.ADDRESS.withTargetLayout(layout), ValueLayout.ADDRESS));
+        var struct = (MemorySegment) getpwnam.invoke(Arena.ofAuto().allocateUtf8String(System.getProperty("user.name")));
+        var field = layout.varHandle(MemoryLayout.PathElement.groupElement("uid"));
+        var uid = (int) field.get(struct);
+        System.out.println("uid: " + uid);
     }
 }
+
